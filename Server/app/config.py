78ca23a3ea8bfba94c8bb39c8a04c9e7121a720d
Shivam Maurya <shivammaurya0248@gmail.com>
2025-08-11 22:05:21 +0530

    Added docker

--- Server/app/config.py
+++ Server/app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     API_KEY: str
     REDIS_URL: str
     REDIS_URL_local: str
-    testing: bool = False
+    testing: bool = True
 
     class Config:
         env_file = Path(__file__).parent / ".env"
